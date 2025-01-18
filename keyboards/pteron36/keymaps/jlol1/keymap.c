50e6a94ff3f2b1c7939866132cd9424fdbd541a8
Juan Luis Ordonez Lucas <jlol84@gmail.com>
2025-01-18 16:18:12 +0200

    restored backspace in thumb

--- keyboards/pteron36/keymaps/jlol1/keymap.c
+++ keyboards/pteron36/keymaps/jlol1/keymap.c
@@ -82,8 +82,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_Q, KC_W, KC_E, KC_R, KC_T, 				KC_Y, KC_U, KC_I, KC_O, KC_P, 
 	KC_A, KC_S, KC_D, KC_F, KC_G, 				KC_H, KC_J, KC_K, KC_L, TD(TD_SCLN_QUOT), 
 	LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, KC_AUDIO_MUTE, 	KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH), 
+<<<<<<< HEAD
 	KC_LALT, MO(1), MO(4), TO(1), 				TO(2), LT(4,KC_LGUI), KC_SPC, LT(2,KC_DEL)
 >>>>>>> 0adca45d00 (layer changes)
+=======
+	KC_LALT, LT(1, KC_BSPC), MO(4), TO(1), 				TO(2), LT(4,KC_LGUI), KC_SPC, LT(2,KC_DEL)
+>>>>>>> db1e1f46e7 (restored backspace in thumb)
 ),
 
 [_MOVE] = LAYOUT_split_3x5_3_encoder(
